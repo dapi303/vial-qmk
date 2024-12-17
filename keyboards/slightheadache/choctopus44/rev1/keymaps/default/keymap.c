8e7f443e9bd96b011df8e5a46c51cb488c44274d
d.piotrowski <dapiotrowski93@gmail.com>
2024-12-18 00:35:00 +0100

    fixed build

--- keyboards/slightheadache/choctopus44/rev1/keymaps/default/keymap.c
+++ keyboards/slightheadache/choctopus44/rev1/keymaps/default/keymap.c
@@ -30,27 +30,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC, \
     KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_ENT,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT), \
-                        KC_LCTRL, KC_LALT,  LOWER,     KC_SPC,             KC_BSPC,  RAISE,    KC_RGUI,  KC_RALT \
+                        KC_LCTL, KC_LALT,  LOWER,     KC_SPC,             KC_BSPC,  RAISE,    KC_RGUI,  KC_RALT \
   ),
-  
+
   [_LOWER] = LAYOUT(
     _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINUS, \
     KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12, \
     _______,  _______,  _______,  _______,  _______,   KC_CAPS,  KC_MPLY,  KC_NUBS,  KC_NUHS,  KC_EQUAL, KC_GRV,   _______,  _______, \
                         _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______ \
   ),
-  
+
   [_RAISE] = LAYOUT(
     _______,  ALGR(KC_8),ALGR(KC_9),KC_UP,  S(KC_8),   S(KC_9),            S(KC_SLSH),KC_SLSH, _______,KC_RBRC,S(KC_RBRC),ALGR(KC_RBRC), \
     KC_GRV,   S(KC_EQL),KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_EQL,             ALGR(KC_MINS),S(KC_MINS),_______,_______,_______, _______, \
     _______,KC_NUBS,S(KC_NUBS),ALGR(KC_NUBS),ALGR(KC_7),ALGR(KC_0),KC_MUTE,S(KC_NUHS),KC_NUHS, _______,  _______,  _______,  _______, \
                         _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______ \
   ),
-  
+
   [_ADJUST] = LAYOUT(
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  KC_PSCR,  KC_SLCK,  KC_PAUSE, XXXXXXX, \
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  KC_PSCR,  KC_SLCT,  KC_PAUSE, XXXXXXX, \
     XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  KC_INS,   KC_HOME,  KC_PGUP,  XXXXXXX, \
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  RESET,    XXXXXXX,  KC_DEL,   KC_END,   KC_PGDN,  XXXXXXX, \
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  KC_DEL,   KC_END,   KC_PGDN,  XXXXXXX, \
                         _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______ \
   ),
 };
