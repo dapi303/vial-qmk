ffb75f0f741e088cfc0486abac8e477ec493c569
d.piotrowski <dapiotrowski93@gmail.com>
2024-12-18 19:56:08 +0100

    switch encoder directin, added missing vial configs

---
+++ keyboards/slightheadache/choctopus44/rev1/keymaps/default/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+
+#define VIAL_KEYBOARD_UID {0xF8, 0xC9, 0xDC, 0xC8, 0xB6, 0x75, 0xFE, 0x08}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 5}
--- keyboards/slightheadache/choctopus44/rev1/keymaps/default/keymap.c
+++ keyboards/slightheadache/choctopus44/rev1/keymaps/default/keymap.c
@@ -28,8 +28,8 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
     KC_ESC,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC, \
-    KC_TAB,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_ENT,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT), \
+    KC_LSFT,   KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+    KC_LCTL,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_ENT,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT), \
                         KC_LCTL, KC_LALT,  LOWER,     KC_SPC,             KC_BSPC,  RAISE,    KC_RGUI,  KC_RALT \
   ),
 
@@ -60,16 +60,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 }
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    switch(biton32(layer_state)) {
-      case _RAISE:
-        clockwise ? tap_code(KC_VOLU) : tap_code(KC_VOLD);
-        break;
-      case _LOWER:
-        clockwise ? tap_code(KC_MNXT) : tap_code(KC_MPRV);
-        break;
-      default:
-        clockwise ? tap_code(KC_PGUP) : tap_code(KC_PGDN);
-        break;
+    if (index == 0) {
+        clockwise ? tap_code(KC_VOLD) : tap_code(KC_VOLU);
     }
-    return true;
+    return false;
 }
